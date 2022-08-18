1c0e8ca66b7a583f79b676c2998d849c1386b311
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-18 22:23:31 +0200

    create Bodypart component

---
+++ src/components/BodyPart.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+
+import Icon from "../assets/icons/gym.png";
+
+const BodyPart = ({ item, setBodypart, bodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={
+        bodyPart === item
+          ? {
+              borderTop: "4px solid #ff2625",
+              backgroundColor: "#fff",
+            }
+          : ""
+      }
+    >
+      <img
+        src={Icon}
+        alt="dumbbell"
+        styles={{ width: "40px", height: "40px" }}
+      />
+    </Stack>
+  );
+};
+
+export default BodyPart;
