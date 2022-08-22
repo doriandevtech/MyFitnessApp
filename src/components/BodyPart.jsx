fa67d62f63f04787639d938bfed5fd9af1d9b516
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2022-08-22 11:47:09 +0200

    change body parts img style

--- src/components/BodyPart.jsx
+++ src/components/BodyPart.jsx
@@ -18,12 +18,24 @@ const BodyPart = ({ item, setBodypart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+      onClick={() => {
+        setBodypart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
     >
       <img
         src={Icon}
         alt="dumbbell"
-        styles={{ width: "40px", height: "40px" }}
+        style={{ width: "40px", height: "40px" }}
       />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
